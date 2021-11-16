dc90e21620613f40ae857c2859adedaf78ff8582
duclapjp <duclapjp>
2021-11-17 00:37:05 +0700

    update method: create, findByDate,Sort

--- src/main/java/com/group4/service/spending/SpendingDAO.java
+++
@@ -1,7 +0,0 @@
-package com.group4.service.spending;
-
-import com.group4.model.financial.Spending;
-import com.group4.service.IService;
-
-public interface SpendingDAO extends IService<Spending> {
-}
